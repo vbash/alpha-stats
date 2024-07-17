a35ed29e042c17946196bd3d485d550c774b8a5f
Vitaliy Bashun <vitaliy.bashun@gmail.com>
2024-07-17 15:57:29 +0300

    Update app.py
    Optimised rating formula

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ def print_rating_table(rating_file_path):
 	)
 
 file_path = "https://storage.googleapis.com/alphastats_ratings/rating_carabin_1_20240707.csv"
-extended_path = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1.parquet"
-extended_path_SAS = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_SAS.parquet"
-extended_path_Lady = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_lady.parquet"
-extended_path_Senior = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_senior.parquet"
-extended_path_Junior = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_junior.parquet"
+extended_path = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_v2.parquet"
+extended_path_SAS = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_SAS_v2.parquet"
+extended_path_Lady = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_lady_v2.parquet"
+extended_path_Senior = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_senior_v2.parquet"
+extended_path_Junior = "https://storage.googleapis.com/alphastats_ratings/extended_rating_1_junior_v2.parquet"
 file_path_matches = "https://storage.googleapis.com/alphastats_ratings/last6matches.csv"
 
 st.title("Рейтинг спортсменів-стрільців. Практична стрільба. Карабін.")
@@ -61,7 +61,7 @@ st.write(fig)
 fig2 = px.histogram(df_formated, x="Клас спортсмена")
 st.write(fig2)
 
-st.write("* для розрахунку рейтингу використовуються данні з останніх 6 матчів національного рівня (Чемпіонат України, Кубок України). Якщо спортсмен пропустив матч, то за такий матч спортсмен отримує свій найгірший результат помножений на 0.75. Далі результати за останні 6 матчів перемножуються на наступні коєфіцієнти: 1.0, 0.9, 0.8, 0.7, 0.6, 0.5, тачим чином, що б матчі, які видбулись останніми, мали вищу вагу. Сума результатів і є рейтингом спортсмена. Максимальний теоретично можливий тейтинг складає 450 балів (потрібно виграти 6 останніх матчів).")
+st.write("* для розрахунку рейтингу використовуються данні з останніх 6 матчів національного рівня (Чемпіонат України, Кубок України). Якщо спортсмен пропустив матч, то за такий матч спортсмен отримує свій середній результат помножений на 0.75. Далі результати за останні 6 матчів перемножуються на наступні коєфіцієнти: 1.0, 0.9, 0.8, 0.7, 0.6, 0.5, тачим чином, що б матчі, які видбулись останніми, мали вищу вагу. Сума результатів і є рейтингом спортсмена. Максимальний теоретично можливий тейтинг складає 450 балів (потрібно виграти 6 останніх матчів).")
 st.write("** для розрахунку класса використовуються відсотки набрані спортсменом у останніх 6 матчах. З них беруться два найкращіх результата і обраховуєтся цередне значення цих двох найкращіх. Далі, відповідно до результатів вихначаються відповідні класи: Grand Master - 95-100%, Master - 85-94.9%, A - 75-84.9%, B - 60-74.9%, C - 40-59.9%, D - 2-39.9%")
 
 
