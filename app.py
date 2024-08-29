f6f2f696216e708d7f4ebab6782638374032b637
Vitaliy Bashun <vitaliy.bashun@gmail.com>
2024-08-29 15:15:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,10 @@ st.set_page_config(page_title="Alpha Stats Carbine", layout="wide")
 
 
 st.title("Рейтинг спортсменів-стрільців. Практична стрільба. Карабін.")
+
+url = "https://alphastatistic.com"
+
+
 st.title("Ми переїхали на нову адресу: alphastatistic.com")
+st.write("check out this [link](%s)" % url)
+st.markdown("check out this [link](%s)" % url)
