4d49e2389452fb27e412d72367f6eef79e880557
Vitaliy Bashun <vitaliy.bashun@gmail.com>
2024-08-29 15:16:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ url = "https://alphastatistic.com"
 
 
 st.title("Ми переїхали на нову адресу: alphastatistic.com")
-st.write("check out this [link](%s)" % url)
+st.write("check out this [alphastatistic.com](%s)" % url)
 st.markdown("check out this [link](%s)" % url)
